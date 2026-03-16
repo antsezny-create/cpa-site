89fe4da5c8a59aa49313264f641f7e38abee759a
antsezny-create <antsezny@gmail.com>
2026-03-16 18:57:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showServices() {
+  document.getElementById("service-detail").style.display = "block";
+  document.getElementById("service-detail").scrollIntoView({ behavior: "smooth" });
+}
+
+function hideServices() {
+  document.getElementById("service-detail").style.display = "none";
+}
+
+function copyEmail() {
+  navigator.clipboard.writeText("sesnyanthony@gmail.com");
+  alert("Email copied to clipboard!");
+}
