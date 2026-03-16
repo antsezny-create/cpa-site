68a264e384d682216f15efbac669b71ab0f9d06c
antsezny-create <antsezny@gmail.com>
2026-03-16 18:53:51 -0400

    Update script.js.js

--- script.js.js
+++ script.js.js
@@ -2,10 +2,11 @@ function showServices() {
   document.getElementById("service-detail").style.display = "block";
   document.getElementById("service-detail").scrollIntoView({ behavior: "smooth" });
 }
-
+ 
 function hideServices() {
   document.getElementById("service-detail").style.display = "none";
 }
+ 
 function copyEmail() {
   navigator.clipboard.writeText("sesnyanthony@gmail.com");
   alert("Email copied to clipboard!");
